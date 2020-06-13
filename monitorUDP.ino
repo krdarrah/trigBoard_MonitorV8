f2720b489658202b401da7b844f85aaafc4c798e
KD Circuits <kevin@kdcircuits.com>
2020-06-13 01:48:49 -0400

    first commit

---
+++ monitorUDP.ino
@@ -0,0 +1,73 @@
+void initMonitorUDP() {
+
+  if (udp.listen(config.udpPort)) {
+    Serial.print("UDP Listening on IP: ");
+    Serial.println(WiFi.localIP());
+    udp.onPacket([](AsyncUDPPacket packet) {// all data comes in here
+      for (int i = 0; i < packet.length(); i++) {
+        newPacket[i] = char(packet.data()[i]);
+        newPacket[i + 1] = NULL;
+      }
+      if (strcmp(newPacket, oldPacketReceived) != 0) {//don't match, so must be new
+        Serial.println("something new");
+        
+        strcpy(oldPacketReceived, newPacket);
+        receivedPackets = 0;
+        firstPacket = true;
+
+        //        //Good debug info
+        //        Serial.print("UDP Packet Type: ");
+        //        Serial.print(packet.isBroadcast() ? "Broadcast" : packet.isMulticast() ? "Multicast" : "Unicast");
+        //        Serial.print(", From: ");
+        //        Serial.print(packet.remoteIP());
+        //        Serial.print(":");
+        //        Serial.print(packet.remotePort());
+        //        Serial.print(", To: ");
+        //        Serial.print(packet.localIP());
+        //        Serial.print(":");
+        //        Serial.print(packet.localPort());
+        //        Serial.print(", Data: ");
+
+        Serial.print("Playing Track:");
+        int trackToPlay = atoi(newPacket);
+        Serial.println(trackToPlay);
+        //here's where we can play the track
+        donePlaying = false;
+        digitalWrite(speakerPwrPin, LOW);
+        sendCommand(CMD_PLAY_FOLDER_FILE, 1 , trackToPlay);
+        delay(100);
+        if (mp3.available())
+        {
+          Serial.println(decodeMP3Answer());
+          mp3.flush();
+        }
+        delay(500);
+        unsigned long mp3StartTime = millis();
+        while (millis() - mp3StartTime < 5000) {
+          sendCommand(CMD_QUERY_STATUS);
+          delay(100);
+          if (mp3.available())
+          {
+            //decodeMP3Answer();
+            Serial.println(decodeMP3Answer());
+            mp3.flush();
+            if (donePlaying) {
+              Serial.println("Done playing");
+              break;
+            }
+          }
+        }
+        delay(100);
+        digitalWrite(speakerPwrPin, HIGH);
+      } else {
+        receivedPackets++;
+        Serial.println(receivedPackets);
+      }
+
+    });
+  }
+
+
+
+
+}
---
+++ trigBoard_MonitorV8.ino
@@ -0,0 +1,62 @@
+//ESP32 Monitor System from UDP
+// I use the ESP32 Feather board from Adafruit
+// amplifier is https://www.amazon.com/gp/product/B07CRVRG83/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1
+// mp3 player https://www.amazon.com/gp/product/B0725RHR4D/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1
+
+
+#include "monitorincludes.h"//special stuff needed just for this setup, mp3 player and pins
+#include "includes.h"
+const char fwVersion[] = "6/13/20 MONITOR";
+
+void setup() {
+  pinMode(LEDpin, OUTPUT);
+  digitalWrite(LEDpin, HIGH);
+  pinMode(speakerPwrPin, OUTPUT);
+  Serial.begin(115200);
+  mp3.begin(9600);
+  delay(500);
+  loadConfiguration(filename, config);
+
+
+  sendCommand(CMD_SEL_DEV, 0, DEV_TF);
+  delay(100);
+  if (mp3.available())
+  {
+    Serial.println(decodeMP3Answer());
+    mp3.flush();
+  }
+  connectWiFi();
+  initMonitorUDP();
+  initBluetooth();
+  bluetoothTimeoutStart = millis();
+}
+
+void loop() {
+
+  if ((millis() - bluetoothTimeoutStart) < (60 * 5 * 1000)) {//kill the bluetooth after 5minutes from boot
+    if (!OTAsetup)
+      serviceBluetooth();
+    else
+      checkOTA();
+  } else {
+    btStop();
+    digitalWrite(LEDpin, LOW);
+    if (WiFi.status() != WL_CONNECTED)
+      ESP.restart();
+  }
+
+
+
+
+  if (strcmp(newPacket, oldPacketReceived) == 0) {//we received new packet, so will start a timer before resetting
+    if (firstPacket) {
+      startTimeForReset = millis();
+      firstPacket = false;
+    }
+    if (millis() - startTimeForReset > resetTime) {
+      firstPacket = true;
+      startTimeForReset = millis();
+      strcpy(oldPacketReceived, "");
+    }
+  }
+}
